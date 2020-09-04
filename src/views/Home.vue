8aea1930e51b57a63593f3be0b3c4f7ba8e7244d
Bagus <solaybagus2@gmail.com>
2020-09-04 11:04:57 +0700

    update1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,291 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="containers">
+    <!-- Navbars -->
+    <div class="navbars">
+      <div class="logos">
+        <img src="https://www.loket.com/images/logo-loket-white.png" alt="">
+      </div>
+      <div class="searchs"></div>
+      <div class="infoacc"></div>
+    </div>
+    <!--  -->
+    <!-- <div class="hashtags">
+      <h1>#Event Online</h1>
+      <h1>#Blog</h1>
+      <h1>#LoketBliblicom</h1>
+    </div> -->
+    <!--Carousel Wrapper-->
+    <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+      <!--Indicators-->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+      </ol>
+      <!--/.Indicators-->
+      <!--Slides-->
+      <div class="carousel-inner" role="listbox">
+        <!--First slide-->
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="https://loket-development.s3.amazonaws.com/images/ss/1596134412_8uFDbG.jpg"
+            alt="First slide">
+        </div>
+        <!--/First slide-->
+        <!--Second slide-->
+        <div class="carousel-item">
+          <img class="d-block w-100" src="https://loket-production-sg.s3.ap-southeast-1.amazonaws.com/images/ss/1598443215_vptrt5.jpg"
+            alt="Second slide">
+        </div>
+        <!--/Second slide-->
+        <!--Third slide-->
+        <div class="carousel-item">
+          <img class="d-block w-100" src="https://loket-production-sg.s3.ap-southeast-1.amazonaws.com/images/ss/1599017003_O4igGH.jpg"
+            alt="Third slide">
+        </div>
+        <!--/Third slide-->
+      </div>
+      <!--/.Slides-->
+      <!--Controls-->
+      <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+      <!--/.Controls-->
+    </div>
+    <!--  -->
+    <!-- Content Event -->
+    <div class="contents">
+      <div class="featureds">
+        <h1>Featured Event</h1>
+        <div class="cardevent">
+          <div class="imagecard">
+            <img src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20200831194010_5f4cefaac1d09.jpg" alt="">
+          </div>
+          <div class="cardcontent">
+            <h1>Lorem ipsum dolor sit amet consectetur</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
+               maxime eum consectetur quasi iste adipisci nam quas esse quaerat
+                laboriosam
+            </p>
+            <div>
+              <router-link to="/ticket">
+                <button class="btn btn-primary">BELI TIKET</button>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="topsells">
+        <h1>Top Selling</h1>
+        <div class="cardsells">
+          <div class="cards" v-for="i in 3" :key="i.id">
+            <div class="imgcards">
+              <img src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20200818181327_5f3bb7d79bb4f.jpg" alt="">
+            </div>
+            <div class="contentcards">
+              <div>
+                <div class="judulcards">
+                  <h1>KOMOIDOUMENOIKOMOIDOUMENOIKOMOIDOUMENOIKOMOIDOUMENOIKOMOIDOUMENOI</h1>
+                </div>
+                <div class="cntncards">
+                  <div class="textcards">
+                    <p>Comika Entertainment</p>
+                    <p>20 December 2020</p>
+                    <p>Istora Senayan, DKI Jakarta</p>
+                  </div>
+                  <div class="buton">
+                    <button class="btn btn-primary">BELI</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="pluscntn bg-info">
+      <div class="imgplus">
+        <img src="https://loket-asset-production.s3.amazonaws.com/loket-live.png" alt="">
+      </div>
+      <div class="cntnplus">
+        <div class="judulplus">
+          <h1>Buat Event Online sekarang lebih mudah di LOKET</h1>
+          <p>Mulai dari acara musik, diskusi, workshop hingga online webinar LOKET
+             sekarang hadir dengan fitur LOKET LIVE
+             untuk event creator yang membutuhkan layanan streaming langsung</p>
+        </div>
+        <!-- <div>
+        </div> -->
+        <div class="btn btn-primary">BUAT EVENT</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+
 };
 </script>
+
+<style lang="scss" scoped>
+  .navbars{
+    width: 100%;
+    height: 70px;
+    background: #152955;
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+  .logos img{
+    width: 150px;
+    margin-left: 30px;
+  }
+  .hashtags{
+    display: flex;
+  }
+  .hashtags h1{
+    padding: 10px 0;
+    margin-left: 20px;
+    font-size: 20px;
+  }
+  .contents{
+    display: flex;
+    justify-content: center;
+    padding: 30px 0;
+    margin-bottom: 30px;
+  }
+  .featureds{
+    display: flex;
+    flex-direction: column;
+  }
+  .featureds h1{
+    font-size: 20px;
+    font-weight: bolder;
+  }
+  .cardevent{
+    width: 472px;
+    height: 410px;
+    background: white;
+    border-radius: 10px;
+    margin-right: 10px;
+    box-shadow: 0 2px 6px 0 rgba(28,29,29,.25);
+  }
+  .topsells{
+    display: flex;
+    flex-direction: column;
+  }
+  .imagecard{
+    width: 100%;
+    height: 225px;
+    object-fit: cover;
+  }
+  .imagecard img{
+    width: 100%;
+    height: 100%;
+    border-top-right-radius: 10px;
+    border-top-left-radius: 10px;
+  }
+  .cardcontent{
+    padding: 15px 10px;
+  }
+  .cardcontent h1{
+    font-weight: bold;
+  }
+  .cardcontent p{
+    font-size: 15px;
+    max-height: 66px;
+  }
+  .topsells h1{
+    font-size: 20px;
+    font-weight: bolder;
+  }
+  .cardsells{
+    width: 472px;
+    height: 410px;
+    background: white;
+    border-radius: 10px;
+    margin-right: 10px;
+    box-shadow: 0 2px 6px 0 rgba(28,29,29,.25);
+  }
+  .cardsells .cards{
+    width: 100%;
+    height: calc(100% /3);
+    padding: 10px;
+    background: white;
+    display: flex;
+    border-bottom: 1px solid #d8d8d8;
+  }
+  .imgcards img{
+    width: 110px;
+    height: 110px;
+    object-fit: cover;
+    border-radius: 10px;
+  }
+  .contentcards{
+    padding: 0 10px;
+    display: flex;
+  }
+  .judulcards h1{
+    font-size: 14px;
+    max-width: 265px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .cntncards{
+    display: flex;
+  }
+  .textcards{
+    max-width: 200px;
+  }
+  .textcards p{
+    font-size: 14px;
+    margin: 0;
+    max-height: 21px;
+    width: 200px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+  .buton{
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    margin-left: 30px;
+  }
+  .buton button{
+    padding: 5px 25px;
+  }
+  .pluscntn{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 400px;
+    margin-top: 60px;
+    .imgplus{
+      width: 500px;
+      img{
+        width: 450px;
+      }
+    }
+    .cntnplus{
+      width: 500px;
+      .judulplus{
+        color: white;
+        h1{
+          font-size: 28px;
+          font-weight: bold;
+          margin-bottom: 20px;
+        }
+        p{
+          font-size: 16px;
+        }
+      }
+    }
+  }
+</style>
---
+++ src/views/Ticket.vue
@@ -0,0 +1,570 @@
+<template>
+  <div class="contains">
+    <!-- Navbars -->
+    <div class="navbars">
+      <div class="logos">
+        <img src="https://www.loket.com/images/logo-loket-white.png" alt="">
+      </div>
+      <div class="searchs"></div>
+      <div class="infoacc"></div>
+    </div>
+    <!--  -->
+    <div class="invoice">
+      <h1>FRESCOllaboration Show (Brisia Jodie x Alif Rizky)</h1>
+      <button class="btn btn-primary">BELI TIKET</button>
+    </div>
+    <div class="ticket">
+      <div class="imgticket">
+        <img src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/banner/20200831194010_5f4cefaac1d09.jpg" alt="">
+      </div>
+      <div class="infoticket">
+        <div class="judulticket">
+          <h1>ANAKBRAND CLASS - MODUL 1 (BRAND STRATEGY) </h1>
+        </div>
+        <hr>
+        <div class="agendaticket">
+          <div class="selenggarakan">
+            <h1>Diselenggarakan Oleh</h1>
+            <div>
+              <img src="https://s3-ap-southeast-1.amazonaws.com/loket-production-sg/images/organization/20200818181404_5f3bb7fc20283.png" alt="">
+              <a href="#">Ideate.id</a>
+            </div>
+          </div>
+          <div class="tglwkt">
+            <h1>Tanggal & Waktu</h1>
+            <p>29 Aug 2020 - 05 Sep 2020</p>
+            <p>10:00 - 17:30 WIB</p>
+          </div>
+          <div class="lokasi">
+            <h1>Lokasi</h1>
+            <p>Event Online</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="deskticket">
+      <div class="chooseopt">
+        <div @click="desk" class="menuclick desk borbot">
+          <h1>DESKRIPSI EVENT</h1>
+        </div>
+        <div @click="cate" class="menuclick cate">
+          <h1>KATEGORI TIKET</h1>
+        </div>
+      </div>
+      <!-- if 1 -->
+      <div class="deskripsi" v-if="code === 1">
+        <div class="juduldesk">
+          <h1>DESKRIPSI EVENT</h1>
+        </div>
+        <div class="textdesk">
+          <p>Brisia Jodie dan Alif Rizky atau biasa dikenal dengan Paijo
+             akan tampil di Frescollaboration Show yang diadakan oleh Kopi
+              Fresco! Alif Rizky yang dikenal sebagai content creator akan
+               bersama Brisia Jodie dalam intimate session yang akan tampil
+                di Frescollaboration Show pada 12 September 2020. Jangan
+                 sampai ketinggalan penampilan dari mereka.</p>
+        </div>
+      </div>
+      <!-- <div class="categori" v-if="code === 2">
+        <article class="card fl-left">
+          <section class="date">
+            <time datetime="23th feb">
+              <span>23</span><span>feb</span>
+            </time>
+          </section>
+          <section class="card-cont">
+            <small>dj khaled</small>
+            <h3>live in sydney</h3>
+            <div class="even-date">
+            <i class="fa fa-calendar"></i>
+            <time>
+              <span>wednesday 28 december 2014</span>
+              <span>08:55pm to 12:00 am</span>
+            </time>
+            </div>
+            <div class="even-info">
+              <i class="fa fa-map-marker"></i>
+              <p>
+                nexen square for people australia, sydney
+              </p>
+            </div>
+            <a href="#">tickets</a>
+          </section>
+        </article>
+      </div> -->
+      <div class="categori" v-if="code === 2">
+        <div class="all">
+          <div class="lefts">
+            <span></span>
+            <h1>28 Aug</h1>
+          </div>
+          <div class="rights">
+            <div class="judulrights">
+              <h1>2-Day Conference (4 Workshops)</h1>
+            </div>
+            <hr>
+            <div class="dipright">
+              <div class="infoasdeq">
+                <p>Access to ANY ONE Workshop: 1. Design Sprint 2.
+                   Product Research 3. UX Design 4. Product Design
+                </p>
+                <a href="#">Berakhir tanggal 05 September 2020 | 10:00</a>
+              </div>
+              <div class="pricer">
+                <h1>GRATIS</h1>
+              </div>
+              <div class="btn btn-primary buy">BELI</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      code: 1,
+      test: 2,
+    };
+  },
+  methods: {
+    cate() {
+      document.querySelector('.desk').classList.remove('borbot');
+      document.querySelector('.cate').classList.add('borbot');
+      this.code = 2;
+    },
+    desk() {
+      document.querySelector('.cate').classList.remove('borbot');
+      document.querySelector('.desk').classList.add('borbot');
+      this.code = 1;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .contains{
+    // padding: 40px 0;
+    display: flex;
+    // justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    .navbars{
+      width: 100%;
+      height: 70px;
+      background: #152955;
+      display: flex;
+      align-items: center;
+      margin-bottom: 10px;
+      .logos img{
+        width: 150px;
+        margin-left: 30px;
+      }
+    }
+    .invoice{
+      z-index: 2;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      height: 100px;
+      background: whitesmoke;
+      h1{
+        font-weight: bolder;
+        font-size: 28px;
+        color: #686868;
+        margin-left: 200px;
+      }
+      .btn{
+        padding: 10px 25px;
+        margin-right: 30px;
+      }
+    }
+    .ticket{
+      width: 900px;
+      display: flex;
+      flex-direction: column;
+      border-radius: 7px;
+      border: 2px solid #d7d7d7;
+      margin-bottom: 60px;
+      .imgticket{
+        img{
+          width: 100%;
+          border-top-left-radius: 7px;
+          border-top-right-radius: 7px;
+        }
+      }
+      .infoticket{
+        padding: 25px 40px;
+        .judulticket{
+          h1{
+            font-size: 32px;
+            width: 100%;
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            color: #686868;
+          }
+        }
+        .agendaticket{
+          display: flex;
+          justify-content: space-between;
+          h1{
+            color: rgb(88, 88, 88);
+            font-size: 18px;
+            font-weight: bolder;
+            margin-bottom: 25px;
+          }
+          p{
+            color: #a7a7a7;
+            font-size: 16px;
+            margin: 0;
+            margin-bottom: 10px;
+          }
+          img{
+            width: 70px;
+            height: 70px;
+            border-radius: 100%;
+          }
+          a{
+            text-decoration: none;
+            font-size: 18px;
+            margin-left: 15px;
+          }
+        }
+      }
+    }
+    .deskticket{
+      width: 900px;
+      .chooseopt{
+        display: flex;
+        display: flex;
+        justify-content: space-around;
+        width: 100%;
+        margin-bottom: 50px;
+        .menuclick{
+          cursor: pointer;
+          display: flex;
+          justify-content: center;
+          width: 50%;
+          padding-bottom: 10px;
+          border-bottom: 2px solid #d7d7d7;
+          h1{
+            font-size: 15px;
+            color: #999;
+            font-weight: bold;
+          }
+        }
+        .menuclick:hover{
+          h1{
+
+          color: rgb(26, 24, 24) !important;
+          }
+        }
+      }
+      .deskripsi{
+        margin-bottom: 100px;
+        .juduldesk{
+          margin-bottom: 25px;
+          h1{
+            font-weight: bold;
+            font-size: 18px;
+            color: #666;
+          }
+        }
+        .textdesk{
+          color: #777;
+          font-weight: normal;
+          font-size: 16px;
+        }
+      }
+      .categori{
+        margin-bottom: 150px;
+        .all{
+          width: 100%;
+          height: 200px;
+          border: 1px solid #dadde6;
+          border-radius: 7px;
+          display: flex;
+          .lefts{
+            width: 24%;
+            border-right: 2px dashed #dadde6;
+            height: 100%;
+            position: relative;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            h1{
+              color: #999;
+            }
+            span{
+              position: absolute;
+              width: 30px;
+              height: 30px;
+              background: white;
+              border-radius: 100%;
+              left: -15px;
+              border-right: 2px solid #dadde6;
+            }
+          }
+          .lefts::before,{
+            content: '';
+            width: 30px;
+            height: 30px;
+            position: absolute;
+            background: white;
+            border-radius: 50%;
+            right: -15px;
+            top: -15px;
+            border-bottom: 1px solid #c2c6d1;
+          }
+          .lefts::after{
+            content: '';
+            width: 30px;
+            height: 30px;
+            position: absolute;
+            background: white;
+            border-radius: 50%;
+            right: -15px;
+            bottom: -15px;
+            border-top: 1px solid #c2c6d1;
+          }
+          .rights{
+            width: 76%;
+            padding: 20px 15px;
+            .judulrights{
+              width: 100%;
+            }
+            h1{
+              color: #686868;
+              font-weight: bold;
+              font-size: 22px;
+            }
+            .dipright{
+              display: flex;
+              .infoasdeq{
+                p{
+                  max-width: 430px;
+                  color: #666;
+                }
+              }
+            }
+            .pricer{
+              align-self: center;
+              margin: 0 15px;
+            }
+            .buy{
+              align-self: center;
+              padding: 5px 35px;
+            }
+          }
+        }
+      }
+    }
+  }
+  .btn{
+    border-radius: 20px;
+    background: transparent;
+    color: black;
+    border: 1px solid black;
+  }
+// asdasd
+  .fl-left{float: left}
+
+.fl-right{float: right}
+
+.container
+{
+  width: 90%;
+  margin: 100px auto
+}
+
+.row{overflow: hidden}
+
+.card
+{
+  display: table-row;
+  width: 100%;
+  background-color: #fff;
+  color: #989898;
+  margin-bottom: 10px;
+  font-family: 'Oswald', sans-serif;
+  text-transform: uppercase;
+  border-radius: 4px;
+  position: relative
+}
+
+.card + .card{margin-left: 2%}
+
+.date
+{
+  display: table-cell;
+  width: 25%;
+  position: relative;
+  text-align: center;
+  border-right: 2px dashed #dadde6
+}
+
+.date:before
+{
+  content: "";
+  display: block;
+  width: 30px;
+  height: 30px;
+  background-color: white;
+  position: absolute;
+  top: -17px ;
+  right: -15px;
+  z-index: 1;
+  border-radius: 50%;
+  border-bottom: 1px solid rgba(0,0,0,.125);
+}
+.date:after{
+  content: "";
+  display: block;
+  width: 30px;
+  height: 30px;
+  background-color: white;
+  position: absolute;
+  top: -15px ;
+  right: -15px;
+  z-index: 1;
+  border-radius: 50%;
+  border-top: 1px solid rgba(0,0,0,.125);
+}
+
+.date:after
+{
+  top: auto;
+  bottom: -15px
+}
+
+.date time
+{
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%)
+}
+
+.date time span{display: block}
+
+.date time span:first-child
+{
+  color: #2b2b2b;
+  font-weight: 600;
+  font-size: 250%
+}
+
+.date time span:last-child
+{
+  text-transform: uppercase;
+  font-weight: 600;
+  margin-top: -10px
+}
+
+.card-cont
+{
+  display: table-cell;
+  width: 75%;
+  font-size: 85%;
+  padding: 10px 10px 30px 50px
+}
+
+.card-cont h3
+{
+  color: #3C3C3C;
+  font-size: 130%
+}
+
+.row:last-child .card:last-of-type .card-cont h3
+{
+  text-decoration: line-through
+}
+
+.card-cont > div
+{
+  display: table-row
+}
+
+.card-cont .even-date i,
+.card-cont .even-info i,
+.card-cont .even-date time,
+.card-cont .even-info p
+{
+  display: table-cell
+}
+
+.card-cont .even-date i,
+.card-cont .even-info i
+{
+  padding: 5% 5% 0 0
+}
+
+.card-cont .even-info p
+{
+  padding: 30px 50px 0 0
+}
+
+.card-cont .even-date time span
+{
+  display: block
+}
+
+.card-cont a
+{
+  display: block;
+  text-decoration: none;
+  width: 80px;
+  height: 30px;
+  background-color: #D8DDE0;
+  color: #fff;
+  text-align: center;
+  line-height: 30px;
+  border-radius: 2px;
+  position: absolute;
+  right: 10px;
+  bottom: 10px
+}
+
+.row:last-child .card:first-child .card-cont a
+{
+  background-color: #037FDD
+}
+
+.row:last-child .card:last-child .card-cont a
+{
+  background-color: #F8504C
+}
+
+@media screen and (max-width: 860px)
+{
+  .card
+  {
+    display: block;
+    float: none;
+    width: 100%;
+    margin-bottom: 10px
+  }
+
+  .card + .card{margin-left: 0}
+
+  .card-cont .even-date,
+  .card-cont .even-info
+  {
+    font-size: 75%
+  }
+}
+// asdasd
+  .borbot{
+    border-bottom: 3px solid #e36a35 !important;
+  }
+</style>
